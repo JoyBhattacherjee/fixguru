92094142b990d3be5040197e77cb8c3a3cb4c365
Joy Bhattacherjee <joybhatta1000@gmail.com>
2023-04-28 20:58:11 +0530

    changed

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 db.sequelize.sync()
